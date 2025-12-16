ff9f066fa6af2317931ea9da55cb1a1143b64b0c
Rishav <pinkyjaiswal7008@gmail.com>
2025-12-16 17:22:24 +0530

    email

--- lib/send-email.ts
+++ lib/send-email.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async ({ to, subject, html }: {to: string, subject: string, html: string}) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: "Maa kali Tours <maakaligroup.com>",
+      from: "Maa kali Tours <info@maakaligroup.com>",
       to: [to],
       subject,
       html,
