c97b3cf44d9455241a12290cf77f970c2b59c694
Rishav <pinkyjaiswal7008@gmail.com>
2025-12-16 17:07:45 +0530

    email

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,7 +3,7 @@ import { adminClient } from "better-auth/client/plugins";
 
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || 'https://maa-kali-entr.vercel.app' ,
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || 'https://maakaligroup.com' ,
     plugins: [
         adminClient()
     ]
